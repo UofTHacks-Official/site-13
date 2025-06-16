8a9619aef24cf043a0593fbba8cbb4b246d5e5d9
Hubert Xu <hubertxu@Huberts-MacBook-Pro.local>
2025-06-15 22:23:24 -0400

    edited stars

--- src/app/components/ui/StarryBackground/StarryBackground.tsx
+++ src/app/components/ui/StarryBackground/StarryBackground.tsx
@@ -25,7 +25,7 @@ const StarryBackground = () => {
           id: i,
           left: `${Math.random() * 100}%`,
           top: `${Math.random() * 100}%`,
-          size: Math.random() * 3 + 3,
+          size: Math.random() * 3 + 2,
           animationDuration: `${Math.random() * 3 + 2}s`,
           animationDelay: `${Math.random() * 5}s`,
         });
